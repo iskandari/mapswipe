58c4977519d162e54ee321873bb77972d77787ac
iskandari <sasha.tedeschi@gmail.com>
2023-04-13 23:45:02 -0400

    added R script for A/A data preparation

--- experiment/experiment.R
+++ experiment/experiment.R
@@ -11,6 +11,18 @@ library(leaflet)
 #Download and place all ARC aggregated results (with geometry)
 #in current working directory
 
+#> files
+#[1] "agg_results_-Mx0_479a3w9FD9oMv5o_geom.geojson" "agg_results_-Mx1EYoYRWMpdXez_lwY_geom.geojson"
+#[3] "agg_results_-MxuKEABaIRO1bvsDGpM_geom.geojson" "agg_results_-MxuOcF64p72sywz07h6_geom.geojson"
+#[5] "agg_results_-MyHuIPUp1dUGxL624Iw_geom.geojson" "agg_results_-MyHxnqRUnxW71eGLs1t_geom.geojson"
+#[7] "agg_results_-Myiz41plx7m5ya_JlCf_geom.geojson" "agg_results_-Myj30YKB4H3uUKBkzbq_geom.geojson"
+#[9] "agg_results_-MyqhHsmKI4yslxt35hn_geom.geojson" "agg_results_-MzMQbpl8xF-cT0yE0H5_geom.geojson"
+#[11] "agg_results_-MzQC3aU6HEP6r-oNRbG_geom.geojson" "agg_results_-MzQE0f5HRdwAheBxqNR_geom.geojson"
+#[13] "agg_results_-MzQGLRXHkiPtemstALI_geom.geojson" "agg_results_-N-VL-_6Tm_H9AkAv94I_geom.geojson"
+#[15] "agg_results_-N01y8krH7ewqrEBdiOI_geom.geojson" "agg_results_-N0b49yCX4V8MBVktKtQ_geom.geojson"
+#[17] "agg_results_-N0b64nUkoZYJW1heMXz_geom.geojson" "agg_results_-N2RucZu1TjB5uo3iJoW_geom.geojson"
+#[19] "agg_results_-N2RwLr1-QWoirplWaRS_geom.geojson" "agg_results_-N2Rz0puWSnCOMQPEh_v_geom.geojson"
+
 files = list.files(path='.')
 
 for (x in 1:length(files)){ 
